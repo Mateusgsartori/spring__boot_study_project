7fdae89ba0af44564e91d8382f5c5aa9ec1868b3
Mateus Sartori <mateusgsartori5@gmail.com>
2024-12-16 19:59:17 -0300

    Add files via upload
    Project update

--- src/main/java/rest_with_spring_boot_and_java/spring/controller/PersonController.java
+++ src/main/java/rest_with_spring_boot_and_java/spring/controller/PersonController.java
@@ -1,16 +1,21 @@
 package rest_with_spring_boot_and_java.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest_with_spring_boot_and_java.spring.service.PersonService;
+import rest_with_spring_boot_and_java.spring.util.MediaType;
 import rest_with_spring_boot_and_java.spring.vo.PersonVO;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/person", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "api/person/v1", produces = {MediaType.APPLICATION_JSON
+        , MediaType.APPLICATION_XML
+        , MediaType.APPLICATION_YAML}
+        , consumes = {MediaType.APPLICATION_JSON
+                    , MediaType.APPLICATION_XML
+                    , MediaType.APPLICATION_YAML})
 public class PersonController {
 
     @Autowired
